c73ec1089742667f883ee8430ba740451fc29302
rockstarcat666 <128318664+rockstarcat666@users.noreply.github.com>
2023-05-18 11:49:37 +0200

    formulario js

--- js/formulario.js
+++ js/formulario.js
@@ -1,3 +1,37 @@
-var formulario = document.getElementsByName('')[0],
-    elementos = formulario.elements,
-    botom
+// En vez de esto tambien se podría haber usado el atributo required en cada uno de los input
+(function () {
+    var formulario = document.getElementsByName('formulario')[0],
+        elementos = formulario.elements,
+        boton = document.getElementById(btn);
+
+    var validarNombre = function (e) {
+        if (formulario.nombre.value == 0) {
+            alert("Completa el campo nombre");
+            e.preventDefault();
+        }
+    }
+
+    var validarEmail = function (e) {
+        if (formulario.email.value == 0) {
+            alert("Completa el campo email");
+            e.preventDefault();
+        }
+    }
+
+    var validarTextarea = function (e) {
+        if (formulario.mensaje.value == 0) {
+            alert("Completa el campo mensaje");
+            e.preventDefault();
+        }
+    }
+
+    var validar = function (e) {
+        validarNombre(e);
+        validarEmail(e);
+        validarTextarea(e);
+    }
+
+    formulario.addEventListener("submit", validar);
+}())
+
+
